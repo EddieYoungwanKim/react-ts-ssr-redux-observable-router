c8f5289f88652f61267a8dae99b2bdaa2ba93248
Eddie Kim <eddie@Eddieui-MacBook-Pro.local>
2019-09-01 02:10:47 +0900

    update

--- apps/example/src/stateManager/root-epic.ts
+++ apps/example/src/stateManager/root-epic.ts
@@ -3,6 +3,4 @@ import { combineEpics } from 'redux-observable';
 import * as todosEpics from './todos/epics';
 import * as routerEpics from './router/epics';
 
-export default combineEpics(
-  ...Object.values({ ...todosEpics, ...routerEpics })
-);
+export default combineEpics(...Object.values({ ...todosEpics, ...routerEpics }));
